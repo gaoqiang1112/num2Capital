41d6adedf9f61b0c63c43ca8571f98c1c363de05
高强 <gaoqiang1112@163.com>
2019-11-21 10:35:41 +0800

    [update] 添加校验及打包代码

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const numberUitl =require('../index')
+const cn1 = numberUitl.num2Capital(123)
+const cn2 = numberUitl.num2Capital(123,'cn_capital')
+const rmb = numberUitl.num2Capital(123,'rmb_capital')
+console.info(cn1)
+console.info(cn2)
+console.info(rmb)
+
